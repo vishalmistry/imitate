0b54508b528469435987d0bd54d1e55d24406252
Vishal Mistry <vishal@vishalmistry.com>
2007-01-26 15:27:08 +0000

    Getting somewhere: got a character device registered

--- module/imitate.c
+++ module/imitate.c
@@ -7,45 +7,159 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/syscalls.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/errno.h>
+#include "syscall_types.h"
+
+/*
+ * Module name for logging
+ */
+#define MODULE_NAME "imitate"
+
+/*
+ * Character device parameters
+ */
+#define DEVICE_NAME "imitate"
+#define DEVICE_MINOR 0
+#define DEVICE_NR_DEVS 1
 
 /*
  * Debug message macros
  */
 #ifdef DEBUG
-#define LOG(msg, args...) (printk(KERN_DEBUG "imitate: " msg "\n", ##args))
+#define DLOG(msg, args...) (printk(KERN_DEBUG MODULE_NAME ": " msg "\n", ##args))
 #else
-#define LOG(msg, args...) /* No Message */
+#define DLOG(msg, args...) /* No Message */
 #endif
 
-static void** sys_call_table = (void**) SYS_CALL_TABLE_ADDR;
+#define LOG(msg, args...) (printk(KERN_INFO MODULE_NAME ": " msg "\n", ##args))
+#define ERROR(msg, args...) (printk(KERN_ERR MODULE_NAME ": " msg "\n", ##args))
+
 
 /*
  * Module information
  */
 MODULE_AUTHOR("Vishal Mistry <vishal@digitalsilver.org>");
 MODULE_DESCRIPTION("Kernel portion of the Imitate record/replay framework");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPLv2");
 
-asmlinkage long (*orig_exit) (int);
+/*
+ * Type definition of a system call
+ */
+typedef void* syscall_t;
+
+/*
+ * System call table and the backup
+ */
+static syscall_t* sys_call_table = (syscall_t*) SYS_CALL_TABLE_ADDR;
+static syscall_t original_sys_call_table[NR_syscalls];
+
+/*
+ * Character device
+ */
+static struct cdev cdev;
+
+/*
+ * Character device registration
+ */
+static struct file_operations fops =
+{
+    .owner   = THIS_MODULE
+};
+
+/*
+ * Register module parameters
+ */
+static ushort dev_major = 0;
+module_param(dev_major, ushort, 0000);
+MODULE_PARM_DESC(dev_major, "Device major number for the " MODULE_NAME " character device");
+
 asmlinkage long handle_sys_exit(int error_code)
 {
-	LOG("sys_exit called");
-	return orig_exit(error_code);
+    LOG("sys_exit called");
+    
+    return ((sys_exit_t) original_sys_call_table[__NR_exit])(error_code);
 }
+
+
+/*
+ * Module function prototypes
+ */
+static int __init kmod_init(void);
+static void __exit kmod_exit(void);
 
 /*
  * Module initialisation function
  */
 static int __init kmod_init(void)
 {
-	LOG("Loaded Imitate kernel module");
-	LOG("SYS_CALL_TABLE_ADDR = %x", SYS_CALL_TABLE_ADDR);
-	LOG("SYS_EXIT = %x", (int) sys_call_table[__NR_exit]);
+    int result, i;
+    dev_t dev;
 
-	orig_exit = sys_call_table[__NR_exit];
-	sys_call_table[__NR_exit] = handle_sys_exit;
+    DLOG("Loading " MODULE_NAME " kernel module");
 
-	return 0;
+    DLOG("SYS_CALL_TABLE_ADDR = %x", SYS_CALL_TABLE_ADDR);
+    DLOG("SYS_EXIT = %x", (int) sys_call_table[__NR_exit]);
+
+    /* Register character device */
+    if (dev_major)
+    {
+        dev = MKDEV(dev_major, DEVICE_MINOR);
+        result = register_chrdev_region(dev, DEVICE_NR_DEVS, DEVICE_NAME);
+    }
+    else
+    {
+        result = alloc_chrdev_region(&dev, DEVICE_MINOR, DEVICE_NR_DEVS, DEVICE_NAME);
+        dev_major = MAJOR(dev);
+    }
+    if (result < 0)
+    {
+        ERROR("Error reserving major number for device");
+        return result;
+    }
+
+    DLOG("Reserved major number %d for device", MAJOR(dev));
+
+    /* Save current system call handlers */
+    for (i = 0; i < NR_syscalls; i++)
+    {
+        original_sys_call_table[i] = sys_call_table[i];
+    }
+
+    /* Hook the system call intercepts */
+    sys_call_table[__NR_exit] = handle_sys_exit;
+
+    /* Set up the character device */
+    cdev_init(&cdev, &fops);
+    cdev.owner = THIS_MODULE;
+    cdev.ops = &fops;
+    result = cdev_add(&cdev, dev, 1);
+    if (result < 0)
+    {
+        ERROR("Error registering character device");
+        goto cdev_add_fail;
+    }
+
+    LOG("Loaded " MODULE_NAME " kernel module");
+
+    return 0;
+
+    /* Handle failures */
+    cdev_add_fail:
+        /* Free character device */
+        kobject_put(&cdev.kobj);
+
+        /* Restore original system call table */
+        for (i = 0; i < NR_syscalls; i++)
+        {
+            sys_call_table[i] = original_sys_call_table[i];
+        }
+        
+        /* Unregister major number */
+        unregister_chrdev_region(dev, DEVICE_NR_DEVS);
+
+        return result;
 }
 
 /*
@@ -53,8 +167,24 @@ static int __init kmod_init(void)
  */
 static void __exit kmod_exit(void)
 {
-	sys_call_table[__NR_exit] = orig_exit;
-	LOG("Unloaded Imitate kernel module");
+    int i;
+    dev_t dev = MKDEV(dev_major, DEVICE_MINOR);
+
+    DLOG("Unloading " MODULE_NAME " module");
+
+    /* Unregister character device */
+    cdev_del(&cdev);
+    
+    /* Restore original system call table */
+    for (i = 0; i < NR_syscalls; i++)
+    {
+        sys_call_table[i] = original_sys_call_table[i];
+    }
+
+    /* Unregister major number */
+    unregister_chrdev_region(dev, DEVICE_NR_DEVS);
+    
+    LOG("Unloaded " MODULE_NAME " module");
 }
 
 /*
